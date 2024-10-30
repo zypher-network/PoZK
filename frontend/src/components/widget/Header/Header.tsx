42a23022305ba8a4f2589e5a832291cf3ce4cedb
GiambiHuang <nash9328@hotmail.com>
2024-10-30 13:44:40 +0800

    fix: user avatar

--- frontend/src/components/widget/Header/Header.tsx
+++ frontend/src/components/widget/Header/Header.tsx
@@ -1,27 +1,28 @@
 "use client";
-import { memo, useMemo } from "react";
+import { memo } from "react";
 import { MemoizedLocation } from "../navbar/navbar";
 import ConnectButton from "../walletButton/ConnectWalletButton";
 import { useSession } from "@/components/hooks/useSession";
 import { useAccount } from "wagmi";
-import { getGradientColors } from "@/lib/avatar";
-import { zeroAddress } from "viem";
+// import { getGradientColors } from "@/lib/avatar";
+// import { zeroAddress } from "viem";
 import { useWeb3Modal } from "@web3modal/wagmi/react";
-
+import UserAvatar from '@/components/icon/user-avatar.svg';
 
 const Header = () => {
   const { hasToken } = useSession();
-  const { isConnected, address } = useAccount();
+  const { isConnected } = useAccount();
   const { open } = useWeb3Modal(); 
 
-  const avatarColor = useMemo(() => getGradientColors(address ?? zeroAddress), [address]);
+  // const avatarColor = useMemo(() => getGradientColors(address ?? zeroAddress), [address]);
   return (
     <div className="flex items-center justify-between min-h-[44px] mt-[44px] mb-[40px]">
       <MemoizedLocation />
       {!isConnected && <ConnectButton />}
       {hasToken && isConnected ? (
-        <div className="cursor-pointer flex justify-center items-center border-2 border-[#FFFFFF33] rounded-full" onClick={() => open({ view: 'Account' })}>
-          <div
+        <div className="cursor-pointer flex justify-center items-center rounded-full" onClick={() => open({ view: 'Account' })}>
+          <UserAvatar className='h-[48px] w-[48px]' />
+          {/* <div
             className="w-[48px] h-[48px] rounded-full"
             style={{
               boxShadow: 'inset 0 0 0 1px rgba(0, 0, 0, 0.1)',
@@ -33,7 +34,7 @@ const Header = () => {
                 radial-gradient(at 29% 88%, ${avatarColor[4]} 0px, transparent 50%)
               `
             }}
-          />
+          /> */}
         </div>
       ) : null}
     </div>
